a573751485042e50613651476abd8a07377c8637
Sujal Thapa <sujalmangar304@gmail.com>
2025-09-10 13:35:32 +0530

    Added my cv and make it downloady by viewers

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "../CSS Components/home.css";
 import github from "../images/github.png";
 import instagram from "../images/instagram.png";
@@ -6,6 +5,22 @@ import linkdin from "../images/linkdin.png";
 import profile from "../images/profile.png";
 
 export default function Home() {
+  const handleDownloadCV = () => {
+    // The path to your CV file located in the `public` folder.
+    const cvFile = "/Sujal Thapa.pdf";
+
+    // Create a temporary link element
+    const link = document.createElement("a");
+    link.href = cvFile;
+
+    // Set the download attribute with a desired filename
+    link.setAttribute("download", "Sujal Thapa CV.pdf");
+
+    // Append the link to the body, click it, and then remove it
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
   return (
     <>
       <main className="mainContainer" id="home">
@@ -27,14 +42,18 @@ export default function Home() {
             </h1>
             <p className="intro">
               A passionate student currently pursuing BCA in cloud computing and
-              Information Technology at Medhavi Skills University in Sikkim. Love to
-              explore new things and upgrade myself in time.
+              Information Technology at Medhavi Skills University in Sikkim.
+              Love to explore new things and upgrade myself in time.
             </p>
-            
+
             <div className="social_Applications">
               <ul className="apps">
                 <li>
-                  <a href="https://github.com/Sujal-Thapa1" target="_blank" rel="noopener noreferrer">
+                  <a
+                    href="https://github.com/Sujal-Thapa1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <img className="logo" src={github} alt="GitHub" />
                   </a>
                 </li>
@@ -44,20 +63,30 @@ export default function Home() {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <img className="instagram" src={instagram} alt="Instagram" />
+                    <img
+                      className="instagram"
+                      src={instagram}
+                      alt="Instagram"
+                    />
                   </a>
                 </li>
                 <li>
-                  <a href="https://t.ly/GlHbn" target="_blank" rel="noopener noreferrer">
+                  <a
+                    href="https://t.ly/GlHbn"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     <img className="logo" src={linkdin} alt="LinkedIn" />
                   </a>
                 </li>
               </ul>
             </div>
-            
+
             <div className="links">
-              <a href="#contact-page">
-                <button className="contactMe">Contact Me</button>
+              <a>
+                <button className="contactMe" onClick={handleDownloadCV}>
+                  Download CV
+                </button>
               </a>
               <button className="Resume">See my Resume</button>
             </div>
